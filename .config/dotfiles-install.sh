db6cdb296a16face47b5d4a322f1123de2de1a60
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-08-31 19:16:53 +0200

    ♻️ Ref: bash function for install script

--- .config/dotfiles-install.sh
+++ .config/dotfiles-install.sh
@@ -1,13 +1,29 @@
-echo ".dotfiles" >> .gitignore
+# echo ".dotfiles" >> .gitignore
+#
+# git clone --bare https://github.com/gitHabbe/dotfiles $HOME/.dotfiles
+#
+# source .habbe_config
+#
+# mkdir -p .config-backup && dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+#
+# dotfiles checkout
+#
+# dotfiles config --local status.showUntrackedFiles no
 
+# -------
+
+cd $HOME
 git clone --bare https://github.com/gitHabbe/dotfiles $HOME/.dotfiles
-
-source .habbe_config
-
-mkdir -p .config-backup && \
-
-dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
 dotfiles checkout
-
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+else
+  echo "Backing up pre-existing dot files.";
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
