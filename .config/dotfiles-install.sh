cf29c786cf62bbc575c7de0a6accbf365d0fc53b
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-09-06 15:19:41 +0200

    🔧 Cfg: install script includes shell packages

--- .config/dotfiles-install.sh
+++ .config/dotfiles-install.sh
@@ -1,21 +1,8 @@
-# echo ".dotfiles" >> .gitignore
-#
-# git clone --bare https://github.com/gitHabbe/dotfiles $HOME/.dotfiles
-#
-# source .habbe_config
-#
-# mkdir -p .config-backup && dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-#
-# dotfiles checkout
-#
-# dotfiles config --local status.showUntrackedFiles no
-
-# -------
-
-# apt-get -y install software-properties-common
-# add-apt-repository ppa:neovim-ppa/unstable
-# apt-get update
-# apt-get -y install git curl zsh build-essential neovim
+apt-get update &&
+apt-get -y install software-properties-common &&
+add-apt-repository -y ppa:neovim-ppa/unstable &&
+apt-get update &&
+apt-get -y install git curl zsh build-essential neovim
 cd $HOME
 git clone --bare https://github.com/gitHabbe/dotfiles $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
@@ -29,3 +16,4 @@ else
 fi;
 dotfiles checkout
 dotfiles config status.showUntrackedFiles no
+source $HOME/.habbe_config
