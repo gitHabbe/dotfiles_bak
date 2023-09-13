56e35aef2d8699fbc1ba1057f06822905e4deeab
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-09-13 08:35:36 +0200

    🐛 Fix: install script supports sh

--- .config/os-packages-install.sh
+++ .config/os-packages-install.sh
@@ -1,18 +1,16 @@
+#!/usr/bin/bash
 apt-get update
 apt-get -y install software-properties-common
 add-apt-repository -y ppa:neovim-ppa/unstable
 apt-get update
 
-install_packages () {
-  local packages=(
-    git
-    curl
-    zsh
-    build-essential
-  )
+install_packages() {
+    local packages="git curl zsh build-essential"
 
-  for package in "${packages[@]}"
-  do
-    apt-get -y install $package
-  done
+    for package in ${packages}; do
+        echo "Installing: $package"
+        apt-get -y install "$package"
+    done
 }
+
+install_packages
