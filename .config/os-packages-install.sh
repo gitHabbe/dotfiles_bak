d27d14290c1d027c218ad6446dd7714080ee2ec3
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-11-14 14:53:03 +0100

    📦 Pkg: install autosuggestions and syntax-highlighting

--- .config/os-packages-install.sh
+++ .config/os-packages-install.sh
@@ -14,3 +14,6 @@ install_packages() {
 }
 
 install_packages
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
