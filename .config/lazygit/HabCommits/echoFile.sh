6eca28bc197630244c18e38dfe12f3c545f85046
Habbe <20054400+gitHabbe@users.noreply.github.com>
2023-11-16 15:20:18 +0100

    🐛 Fix: bash file to echo commit type

--- .config/lazygit/HabCommits/echoFile.sh
+++ .config/lazygit/HabCommits/echoFile.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-echo "print"
 
-## declare an array variable
-emojis=("😊" "😎" "👍")
-
-for emoji in "${emojis[@]}"; do
-#  printf "\U%04x" $(echo "$emoji" | cut -d '' -f 2-4)
-  echo "$(echo "$emoji" | cut -d '' -f 2-4)"
-done
+echo $(echo "$@" | sed -r 's/^.*\(([^a-zA-Z]*([a-zA-Z]*))\).*$/\2/')
